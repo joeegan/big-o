1f68410011e3501fd4aa10f8be3841576502dfe8
Joe Egan <joeegan84@gmail.com>
2017-01-11 22:51:20 +0000

    :chart_with_upwards_trend: move to google charts

--- app.js
+++ app.js
@@ -12,4 +12,32 @@ function itemInList(itemToVerify, list) {
   return iterationCount;
 }
 
-console.log(itemInList(0, [1,5,2]));
+function oN(n) {
+  const results = [['Operations', 'Array length']];
+  for (var i = 0; i < n+1; i++) {
+    results.push([i, itemInList(i, new Array(i))]);
+  }
+  return results;
+}
+
+function chart(element, results) {
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable(results);
+    var options = {
+      title: 'O(n) runtime, Linear',
+      hAxis: {title: 'Length of array', minValue: 0, maxValue: 20},
+      vAxis: {title: 'Operations', minValue: 0, maxValue: 150},
+      legend: 'none'
+    };
+
+    var chart = new google.visualization.ScatterChart(element);
+
+    chart.draw(data, options);
+  }
+}
+
+window.onload = function() {
+  chart(document.getElementById('chart_div'), oN(20));
+}
