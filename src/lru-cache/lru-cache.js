fbb8560140c373cdb5b1f20a36b64c0321d82e07
Joe Egan <joeegan84@gmail.com>
2017-08-13 21:42:25 +0100

    add some util methods

--- src/lru-cache/lru-cache.js
+++ src/lru-cache/lru-cache.js
@@ -1,11 +1,11 @@
 export default class LruCache {
-  constructor(opts) {
-    this.max = opts.max
+  constructor(opts = {}) {
+    this.max = opts.max || Infinity
     this.cache = []
   }
 
   get(key) {
-    const match = this.cache.find(c => c.key === key)
+    const match = this._getMatch(key)
     if (!match) {
       return
     }
@@ -14,15 +14,41 @@ export default class LruCache {
     return match.value
   }
 
-  getLength() {
-    return this.cache.length
-  }
-
   set(key, value) {
     if (this.cache.length === this.max) {
       this.cache.shift()
     }
     this.cache.push({ key, value })
+  }
+
+  keys() {
+    return this.cache.map(c => c.key)
+  }
+
+  values() {
+    return this.cache.map(c => c.value)
+  }
+
+  _getMatch(key) {
+    return this.cache.find(c => c.key === key)
+  }
+
+  peek(key) {
+    const match = this._getMatch(key)
+    if (!match) {
+      return
+    }
+    return match.value
+  }
+
+  del(key) {
+    const match = this._getMatch(key)
+    const position = this.cache.indexOf(match)
+    this.cache.splice(position, 1)
+  }
+
+  get length() {
+    return this.cache.length
   }
 
   reset() {
--- src/lru-cache/lru-cache.test.js
+++ src/lru-cache/lru-cache.test.js
@@ -8,7 +8,7 @@ test('removes the last read once the max is reached', () => {
   lruCache.set('qux', 1)
   lruCache.set('quux', 1)
   lruCache.set('quuux', 1)
-  expect(lruCache.getLength()).toEqual(5)
+  expect(lruCache.length).toEqual(5)
   expect(lruCache.get('foo')).toEqual(undefined)
   expect(lruCache.get('bar')).toEqual(1)
 })
@@ -24,11 +24,44 @@ test(`get's affect caching`, () => {
   expect(lruCache.get('baz')).toEqual(1)
 })
 
+test(`peeking does not affect caching`, () => {
+  const lruCache = new LruCache({ max: 2 })
+  lruCache.set('foo', 1)
+  lruCache.set('bar', 1)
+  lruCache.peek('foo')
+  lruCache.set('baz', 1)
+  expect(lruCache.get('foo')).toEqual(undefined)
+  expect(lruCache.get('bar')).toEqual(1)
+  expect(lruCache.get('baz')).toEqual(1)
+})
+
 test(`empties upon reset`, () => {
-  const lruCache = new LruCache({ max: 3 })
+  const lruCache = new LruCache()
   lruCache.set('foo', 1)
   lruCache.set('bar', 1)
   lruCache.set('baz', 1)
   lruCache.reset()
-  expect(lruCache.getLength()).toEqual(0)
+  expect(lruCache.length).toEqual(0)
+})
+
+test(`deleting objects`, () => {
+  const lruCache = new LruCache()
+  lruCache.set('foo', 1)
+  lruCache.set('bar', 1)
+  lruCache.del('foo')
+  expect(lruCache.length).toEqual(1)
+})
+
+test(`exposes keys`, () => {
+  const lruCache = new LruCache()
+  lruCache.set('foo', 1)
+  lruCache.set('bar', 1)
+  expect(lruCache.keys()).toEqual(['foo', 'bar'])
+})
+
+test(`exposes values`, () => {
+  const lruCache = new LruCache()
+  lruCache.set('foo', 1)
+  lruCache.set('bar', 2)
+  expect(lruCache.values()).toEqual([1, 2])
 })
